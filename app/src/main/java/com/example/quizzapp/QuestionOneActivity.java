7aef6f065bbf0f376a03505f478f4e9f68c9baab
luispipe <lflondonor@unal.edu.co>
2023-08-16 12:06:06 -0300

    first commit

---
+++ app/src/main/java/com/example/quizzapp/QuestionOneActivity.java
@@ -0,0 +1,14 @@
+package com.example.quizzapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_one_main);
+    }
+}
